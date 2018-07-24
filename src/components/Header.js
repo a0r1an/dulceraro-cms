fe4b36fc80f7cffa36f8b02a76ac28d2a945b8cd
adrianeufracio <eufracioadrian@gmail.com>
2018-07-23 23:02:07 -0700

    adds layout and content type of podcast

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 2.5em;
+  text-align: center;
+  font-weight: 900;
+  margin: 0;
+  padding: 0.5em 0;
+`;
+
+const Link = ({ className, children }) => (
+  <a href="/" className={className}>
+    {children}
+  </a>
+)
+
+const StyledLink = styled(Link)`
+  color: #ea40dc;
+  text-decoration: none;
+`;
+class Header extends React.Component {
+  render() {
+    return <section>
+
+      <Title><StyledLink>dulce raro</StyledLink></Title>
+
+    </section>;
+  }
+}
+
+export default Header;
